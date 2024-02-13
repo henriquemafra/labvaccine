8fd4a3a90c15243bcac12ee9649964a14130e78f
Henrique Mafra <henriquemafra89@gmail.com>
2024-02-13 16:58:49 -0300

    FIRST COMMIT

---
+++ src/main/java/com/devinpharmacy/labvaccine/LabvaccineApplication.java
@@ -0,0 +1,13 @@
+package com.devinpharmacy.labvaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabvaccineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabvaccineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinpharmacy/labvaccine/LabvaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinpharmacy.labvaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabvaccineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
